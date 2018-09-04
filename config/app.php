eebfa5d28d4ef983df417cf26f1d4d95e5c85548
Aileni <aileni.tee@glyphgames.com>
2018-09-04 13:38:24 +0800

    https protocol

--- config/app.php
+++ config/app.php
@@ -160,6 +160,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Cyvelnet\EasyCart\EasyCartServiceProvider::class,
+        Ecrmnn\LaravelHttps\Providers\ServiceProvider::class,
     ],
 
     /*
