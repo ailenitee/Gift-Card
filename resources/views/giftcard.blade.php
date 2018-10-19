8bbb85c081e6834e1e0f10f8e2378234558f23ed
Aileni <aileni.tee@glyphgames.com>
2018-10-19 16:40:07 +0800

    allgiftcards add to cart

--- resources/views/giftcard.blade.php
+++ resources/views/giftcard.blade.php
@@ -74,92 +74,48 @@
           </div>
           <div class="col-md-1"></div>
           <div class="col-md-2"></div>
-          
           <div class="col-md-8">
-            <ul class="nav nav-pills mb-3 send-pills" id="pills-tab" role="tablist">
-              <li class="nav-item p-item" id="pillsEmail">
-                <a class="nav-link" id="pills-profile-tab" data-toggle="pill" role="tab" aria-selected="false">
-                  <i class="far fa-comment"></i>
-                </a>
-                <h3>DELIVER</h3>
-              </li>
-              <li class="nav-item p-item" id="pillsDeliver">
-                <a class="nav-link" data-toggle="pill" role="tab" aria-selected="true">
-                  <i class="fas fa-at"></i>
-                </a>
-                <h3>SMS</h3>
-              </li>
-            </ul>
-            <div class="tab-content send-tab-content " id="pills-tabContent">
-              @if($edit == 'edit')
-              @if($address == '')
-              <input type="hidden" name="option" value="sms" class="option">
-              @else
-              <input type="hidden" name="option" value="deliver" class="option">
-              @endif
-              @else
-              <input type="hidden" name="option" value="deliver" class="option">
-              @endif
-              @if($edit == 'edit')
-              <div class="tab-pane show active" id="pillsEmailContent" role="tabpanel" aria-labelledby="pills-home-tab">
-                @else
-                <div class="tab-pane show active email-content" id="pillsEmailContent" role="tabpanel" aria-labelledby="pills-home-tab">
-                  @endif
-                  <div class="form-group">
-                    <label>Sender Name</label>
-                    <input type="text" class="form-control sender" value="{{$sender ? $sender : ''}}" name="" novalidate>
-                  </div>
-                  <div class="form-group">
-                    <label>Recipient's Name</label>
-                    <input type="name" class="form-control name" name="name" value="{{$name ? $name : ''}}">
-                  </div>
-                  <div class="form-group">
-                    <label>Recipient's Address</label>
-                    <input type="text" class="form-control " name="address" value="{{$address ? $address : ''}}">
-                  </div>
-                  <div class="form-group">
-                    <label>Recipient's Mobile</label>
-                    <input type="number" class="form-control mobile" name="mobile" value="{{$mobile ? $mobile : ''}}">
-                  </div>
-                </div>
-                @if($edit == 'edit')
-                <div class="tab-pane" id="pillsDeliverContent" role="tabpanel" aria-labelledby="pills-contact-tab">
-                  @else
-                  <div class="tab-pane deliver-content" id="pillsDeliverContent" role="tabpanel" aria-labelledby="pills-contact-tab">
-                    @endif
-                    <div class="form-group">
-                      <label>Sender Name</label>
-                      <input type="text" class="form-control sender" value="{{$sender ? $sender : ''}}" name="" novalidate>
-                    </div>
-                    <div class="form-group">
-                      <label>Recipient's Name</label>
-                      <input type="name" class="form-control name" name="" value="{{$name ? $name : ''}}">
-                    </div>
-                    <div class="form-group">
-                      <label>Recipient's Mobile</label>
-                      <input type="number" class="form-control mobile" name="" value="{{$mobile ? $mobile : ''}}">
-                    </div>
-                  </div>
-                </div>
+            <div class="r-details">
+              <div class="form-group">
+                <label>Sender Name</label>
+                <input type="text" class="form-control" value="@if(Auth::user()){{ Auth::user()->name }}@else{{$sender}}@endif" name="sender" required>
+              </div>
+              <div class="form-group">
+                <label>Recipient's Name</label>
+                <input type="name" class="form-control" name="name" value="{{$name ? $name : ''}}">
+              </div>
+              <div class="form-group">
+                <label>Recipient's Address</label>
+                <input type="text" class="form-control" name="address" value="{{$address ? $address : ''}}">
+              </div>
+              <div class="form-group">
+                <label>Recipient's Mobile</label>
+                <input type="number" class="form-control" name="mobile" value="{{$mobile ? $mobile : ''}}">
               </div>
             </div>
-            <div class="r-details" style="margin-top:0;">
+          </div>
+          <div class="col-md-2"></div>
+          <div class="col-md-12">
+            <div class="r-details">
+              @if($edit == 'edit')
               <div class="col-sm-6">
-                @if($edit == 'edit')
                 <button type="submit" class="btn-border btn-center" value="update" name='submitbutton'>UPDATE CART</button>
-                @else
-                <button type="submit" class="btn-border btn-center" value="save" name='submitbutton'>ADD TO CART</button>
-                @endif
               </div>
               <div class="col-sm-6">
-                @if($edit == 'edit')
-                <button type="submit" class="btn-red btn-center" value="update_cart" name='submitbutton'>ADD AND CONFIRM ORDER</button>
-                @else
-                <button type="submit" class="btn-red btn-center" value="save_cart" name='submitbutton'>ADD AND CONFIRM ORDER</button>
-                @endif
+                <button type="submit" class="btn-red btn-center" name='submitbutton' value="update_cart">CONFIRM AND CHECKOUT</button>
               </div>
+              @else
+              <div class="col-sm-6">
+                <button type="submit" class="btn-border btn-center disabled" style="background-color: #ddd; border:1px solid #ddd;">ADD TO CART</button>
+                <button type="submit" class="btn-border btn-center n_disabled" value="save" name='submitbutton'>ADD TO CART</button>
+              </div>
+              <div class="col-sm-6">
+                <button type="submit" class="btn-red btn-center disabled" style="background-color: #ddd; border:1px solid #ddd;">CONFIRM AND CHECKOUT</button>
+                <button type="submit" class="btn-red btn-center n_disabled" name='submitbutton' value="save_cart">CONFIRM AND CHECKOUT</button>
+              </div>
+              @endif
+
             </div>
-            <br><br>
           </div>
         </div>
       </form>
